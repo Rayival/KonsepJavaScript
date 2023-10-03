6abb6e8fdb3300bc07abb2a10e7bd1454ac2a108
Rayival Dede Saputra <113332359+Rayival@users.noreply.github.com>
2023-10-03 11:07:30 +0700

    perhitungan.js

--- perhitungan.js
+++ perhitungan.js
@@ -1,8 +1,8 @@
 let x = 5
 let y = 10
 
-let penjumlahan = x+y
-let pengurangan = x-y
+console.log("penjumlahan : "+(x+y))
+console.log("pengurangan : "+(x-y))
 console.log("perkalian : "+(x*y))
 console.log("pembagian : "+(x/y))
 console.log("modulus : "+(x%y))
